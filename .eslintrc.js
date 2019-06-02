2ff5a566cc3cdfe74be3fb7ed6c1cbed021e8000
Héctor Molinero Fernández <hector@molinero.dev>
2019-06-02 11:08:07 +0200

    Updated .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,11 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
 	root: true,
-	env: {
-		node: true
-	},
+	parserOptions: { parser: 'babel-eslint' },
 	extends: ['plugin:vue/essential', '@vue/prettier'],
+	env: { node: true },
 	rules: {
-		'no-console': [isProduction ? 'error' : 'warn'],
-		'no-debugger': [isProduction ? 'error' : 'warn'],
+		'no-console': ['warn', { allow: ['warn', 'error'] }],
 		'prettier/prettier': ['error', { singleQuote: true, useTabs: true }]
-	},
-	parserOptions: {
-		parser: 'babel-eslint'
 	}
 };
