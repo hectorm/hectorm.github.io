a1fac779608b4474755470c78fb0927ec76793ed
Héctor Molinero Fernández <hector@molinero.dev>
2021-09-19 11:09:44 +0200

    Updated dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,8 @@
 module.exports = {
 	root: true,
 	env: { es2021: true, browser: true, node: true },
-	extends: ['plugin:vue/vue3-recommended', '@vue/prettier/recommended'],
+	extends: ['plugin:vue/vue3-recommended', 'plugin:prettier/recommended'],
 	rules: {
 		'no-console': ['warn', { allow: ['warn', 'error'] }],
-		'prettier/prettier': ['error'],
 	},
 };
