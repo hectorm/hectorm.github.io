73bb2e42975feeac377b481a8cca1d15c4a21f72
Héctor Molinero Fernández <hector@molinero.dev>
2023-07-31 16:40:18 +0200

    Format code with new ESLint rules

--- src/pages/index/components/HButton.vue
+++ src/pages/index/components/HButton.vue
@@ -17,7 +17,7 @@ const href = computed(() => {
     // Super-secure quantum-based data encryption algorithm!!1!!111
     href = href.replace(/[a-z]/gi, (s) => {
       return String.fromCharCode(
-        s.charCodeAt(0) + (s.toLowerCase() < "n" ? 13 : -13)
+        s.charCodeAt(0) + (s.toLowerCase() < "n" ? 13 : -13),
       );
     });
   }
